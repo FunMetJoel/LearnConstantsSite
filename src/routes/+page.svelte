a3cfadbd8f5b05a24da178724f8b11ee93a524ae
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2026-07-23 21:01:26 +0200

    Added way to start multiplier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script lang="js">
+  let multiplayer = $state(false);
+</script>
 
 <section class="w-full space-y-8 text-center">
   <header class="space-y-4">
@@ -12,12 +15,23 @@
     </p>
   </header>
 
+  <div class="flex items-center justify-center gap-3">
+    <label class="inline-flex items-center gap-2 text-slate-700 dark:text-slate-300">
+      <input
+        type="checkbox"
+        bind:checked={multiplayer}
+        class="h-5 w-5 rounded border-slate-300 text-sky-500 focus:ring-sky-500"
+      />
+      Multiplayer
+    </label>
+  </div>
+
   <div class="grid gap-4 sm:grid-cols-2 w-full">
-    <a href="/pi" class="p-6 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-2xl shadow-sm hover:border-slate-700 hover:drop-shadow-sky-900 drop-shadow-md">
+    <a href={multiplayer ? "/challange/pi" : "/pi" } class="p-6 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-2xl shadow-sm hover:border-slate-700 hover:drop-shadow-sky-900 drop-shadow-md">
       <h3 class="font-bold">π</h3>
       <p class="text-sm opacity-80">3.141592653589793238462643383...</p>
     </a>
-    <a href="/e" class="p-6 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-2xl shadow-sm hover:border-slate-700 hover:drop-shadow-sky-900 drop-shadow-md">
+    <a href={multiplayer ? "/challange/e" : "/e" } class="p-6 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-2xl shadow-sm hover:border-slate-700 hover:drop-shadow-sky-900 drop-shadow-md">
       <h3 class="font-bold">e</h3>
       <p class="text-sm opacity-80">2.718281828459045235360287471...</p>
     </a>
